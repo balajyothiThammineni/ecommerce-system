7e08cd9925743f1ccc1e5a7c653b46089d746181
balajyothiThammineni <105962124+balajyothiThammineni@users.noreply.github.com>
2023-11-14 20:53:33 +0530

    API added in category

--- SpringBootEcommerce/src/main/java/com/springboot/ecommerce/controller/CategoryController.java
+++ SpringBootEcommerce/src/main/java/com/springboot/ecommerce/controller/CategoryController.java
@@ -1,14 +1,20 @@
+
 package com.springboot.ecommerce.controller;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.ecommerce.exception.InvalidIdException;
 import com.springboot.ecommerce.model.Category;
+import com.springboot.ecommerce.model.Customer;
 import com.springboot.ecommerce.service.CategoryService;
 
 @RestController
@@ -22,9 +28,28 @@ public class CategoryController {
 	}
 	
 	@GetMapping("/category/getall")
-    public List<Category> getAllCategoryr(){
+    public List<Category> getAllCategory(){
 		return categoryService.getAll();
 	}
 	
+	
+	@DeleteMapping("/category/delete/{id}") /* 8080/customer/delete/13 */
+	public ResponseEntity<?> deleteCategory(@PathVariable("id") int id) {
 
+		try {
+			// validate id
+			Category category = categoryService.getCategoryById(id);
+			// delete
+			categoryService.deleteCategory(category);
+			return ResponseEntity.ok().body(" deleted successfully");
+		} catch (InvalidIdException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
+	}
+	
+		
 }
+	
+
+
--- SpringBootEcommerce/src/main/java/com/springboot/ecommerce/service/CategoryService.java
+++ SpringBootEcommerce/src/main/java/com/springboot/ecommerce/service/CategoryService.java
@@ -1,10 +1,12 @@
 package com.springboot.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springboot.ecommerce.exception.InvalidIdException;
 import com.springboot.ecommerce.model.Category;
 import com.springboot.ecommerce.repository.CategoryRepository;
 
@@ -21,8 +23,24 @@ public class CategoryService {
 	
 public List<Category> getAll() {
 		
-		return categoryRepository.findAll();	}
+		return categoryRepository.findAll();	
+		}
 
+   public Category getCategoryById(int id) throws InvalidIdException{
+	Optional<Category> optional = categoryRepository.findById(id);
+	if(!optional.isPresent())
+		throw new InvalidIdException(" Id Invalid");
+    Category category = optional.get();
+	return category;
+}
+
+public void deleteCategory(Category category) {
+	categoryRepository.delete(category);
+}
+
+
+
+   
 
 
 }
